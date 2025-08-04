2dfee86aa55f63fd471bd013f44eb50248bdec82
xlee1327-ops <xlee1327@gmail.com>
2025-08-04 13:16:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json, os, re
 from datetime import datetime, timedelta
 from keep_alive import keep_alive
 
-TOKEN = "MTQwMDgyNjc4NjI4NzMyNTI3Ng.GdufzT.3IC42ISbwxTOEaFnPXkQQM2Xdn_37u3gTv_zE0"
+TOKEN = "TOKEN"
 DONATE_CHANNEL_ID = 1396728149798289519
 LEVI_ID = 854982878869717002
 HUNTER_ROLE_NAME = "Hunters"
